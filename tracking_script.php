0a971c4450213265ada6204c9e07c88a849c0448
unknown <christian@webdev200.com>
2021-01-26 15:06:30 +0800

    feedback and dashboard modal added

---
+++ tracking_script.php
@@ -0,0 +1,8 @@
+<script async>
+    $(document).ready(function() {
+        var activeIcon = $('.track-in-progress svg').clone();
+        var activeTrack =  $('#tracking-active');
+        activeTrack.empty();
+        activeTrack.html(activeIcon);
+    })
+</script>
