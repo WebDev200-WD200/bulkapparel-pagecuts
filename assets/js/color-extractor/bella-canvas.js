f59dc267984b66f6860383aa4531715029d62d4e
christianwebdev200 <73317737+christianwebdev200@users.noreply.github.com>
2022-07-15 14:14:13 +0800

    bella canvas scrap data

---
+++ assets/js/color-extractor/bella-canvas.js
@@ -0,0 +1,668 @@
+var bellaColors = [{
+    "color_alias": "Blue",
+    "color_xref": "Aqua"
+},
+{
+    "color_alias": "Blue",
+    "color_xref": "Atlantic"
+},
+{
+    "color_alias": "Blue",
+    "color_xref": "Baby Blue"
+},
+{
+    "color_alias": "Blue",
+    "color_xref": "Blue"
+},
+{
+    "color_alias": "Blue",
+    "color_xref": "Columbia Blue"
+},
+{
+    "color_alias": "Blue",
+    "color_xref": "Deep Teal"
+},
+{
+    "color_alias": "Blue",
+    "color_xref": "Denim"
+},
+{
+    "color_alias": "Blue",
+    "color_xref": "Dusty Blue"
+},
+{
+    "color_alias": "Blue",
+    "color_xref": "Heather Blue Lagoon"
+},
+{
+    "color_alias": "Blue",
+    "color_xref": "Ice Blue"
+},
+{
+    "color_alias": "Blue",
+    "color_xref": "Lavender Blue"
+},
+{
+    "color_alias": "Blue",
+    "color_xref": "Light Blue"
+},
+{
+    "color_alias": "Blue",
+    "color_xref": "Midnight"
+},
+{
+    "color_alias": "Blue",
+    "color_xref": "Midnight Navy"
+},
+{
+    "color_alias": "Blue",
+    "color_xref": "Navy"
+},
+{
+    "color_alias": "Blue",
+    "color_xref": "Neon Blue"
+},
+{
+    "color_alias": "Blue",
+    "color_xref": "Ocean Blue"
+},
+{
+    "color_alias": "Blue",
+    "color_xref": "Seafoam Blue"
+},
+{
+    "color_alias": "Blue",
+    "color_xref": "Slate"
+},
+{
+    "color_alias": "Blue",
+    "color_xref": "Steel Blue"
+},
+{
+    "color_alias": "Blue",
+    "color_xref": "Team Navy"
+},
+{
+    "color_alias": "Blue",
+    "color_xref": "True Royal"
+},
+{
+    "color_alias": "Blue",
+    "color_xref": "True Royal Acid Wash"
+},
+{
+    "color_alias": "Blue",
+    "color_xref": "Turquoise"
+},
+{
+    "color_alias": "Green",
+    "color_xref": "Dark Olive"
+},
+{
+    "color_alias": "Green",
+    "color_xref": "Dusty Blue"
+},
+{
+    "color_alias": "Green",
+    "color_xref": "Emerald"
+},
+{
+    "color_alias": "Green",
+    "color_xref": "Evergreen"
+},
+{
+    "color_alias": "Green",
+    "color_xref": "Forest"
+},
+{
+    "color_alias": "Green",
+    "color_xref": "Grass Green"
+},
+{
+    "color_alias": "Green",
+    "color_xref": "Green"
+},
+{
+    "color_alias": "Green",
+    "color_xref": "Heather Green"
+},
+{
+    "color_alias": "Green",
+    "color_xref": "Kelly"
+},
+{
+    "color_alias": "Green",
+    "color_xref": "Leaf"
+},
+{
+    "color_alias": "Green",
+    "color_xref": "Lime"
+},
+{
+    "color_alias": "Green",
+    "color_xref": "Military Green"
+},
+{
+    "color_alias": "Green",
+    "color_xref": "Mint"
+},
+{
+    "color_alias": "Green",
+    "color_xref": "Moss Green"
+},
+{
+    "color_alias": "Green",
+    "color_xref": "Neon Green"
+},
+{
+    "color_alias": "Green",
+    "color_xref": "Olive"
+},
+{
+    "color_alias": "Green",
+    "color_xref": "Sage"
+},
+{
+    "color_alias": "Green",
+    "color_xref": "Sea Green"
+},
+{
+    "color_alias": "Green",
+    "color_xref": "Spring Green"
+},
+{
+    "color_alias": "Green",
+    "color_xref": "Synthetic Green"
+},
+{
+    "color_alias": "Green",
+    "color_xref": "Teal"
+},
+{
+    "color_alias": "Neutral/Brown",
+    "color_xref": "Brown"
+},
+{
+    "color_alias": "Neutral/Brown",
+    "color_xref": "Chestnut"
+},
+{
+    "color_alias": "Neutral/Brown",
+    "color_xref": "Cream"
+},
+{
+    "color_alias": "Neutral/Brown",
+    "color_xref": "Dust"
+},
+{
+    "color_alias": "Neutral/Brown",
+    "color_xref": "Espresso"
+},
+{
+    "color_alias": "Neutral/Brown",
+    "color_xref": "Natural"
+},
+{
+    "color_alias": "Neutral/Brown",
+    "color_xref": "Oat"
+},
+{
+    "color_alias": "Neutral/Brown",
+    "color_xref": "Oatmeal"
+},
+{
+    "color_alias": "Neutral/Brown",
+    "color_xref": "Pebble"
+},
+{
+    "color_alias": "Neutral/Brown",
+    "color_xref": "Sand Dune"
+},
+{
+    "color_alias": "Neutral/Brown",
+    "color_xref": "Soft Cream"
+},
+{
+    "color_alias": "Neutral/Brown",
+    "color_xref": "Stone"
+},
+{
+    "color_alias": "Neutral/Brown",
+    "color_xref": "Tan"
+},
+{
+    "color_alias": "Neutral/Brown",
+    "color_xref": "Terracotta"
+},
+{
+    "color_alias": "Neutral/Brown",
+    "color_xref": "Toast"
+},
+{
+    "color_alias": "Orange/Yellow",
+    "color_xref": "Autumn"
+},
+{
+    "color_alias": "Orange/Yellow",
+    "color_xref": "Burnt Orange"
+},
+{
+    "color_alias": "Orange/Yellow",
+    "color_xref": "Citron"
+},
+{
+    "color_alias": "Orange/Yellow",
+    "color_xref": "Coral"
+},
+{
+    "color_alias": "Orange/Yellow",
+    "color_xref": "French Vanilla"
+},
+{
+    "color_alias": "Orange/Yellow",
+    "color_xref": "Gold"
+},
+{
+    "color_alias": "Orange/Yellow",
+    "color_xref": "Heather Marmalade"
+},
+{
+    "color_alias": "Orange/Yellow",
+    "color_xref": "Maize Yellow"
+},
+{
+    "color_alias": "Orange/Yellow",
+    "color_xref": "Mustard"
+},
+{
+    "color_alias": "Orange/Yellow",
+    "color_xref": "Neon Orange"
+},
+{
+    "color_alias": "Orange/Yellow",
+    "color_xref": "Neon Yellow"
+},
+{
+    "color_alias": "Orange/Yellow",
+    "color_xref": "Orange"
+},
+{
+    "color_alias": "Orange/Yellow",
+    "color_xref": "Pale Yellow"
+},
+{
+    "color_alias": "Orange/Yellow",
+    "color_xref": "Strobe"
+},
+{
+    "color_alias": "Orange/Yellow",
+    "color_xref": "Sunset"
+},
+{
+    "color_alias": "Orange/Yellow",
+    "color_xref": "Yellow"
+},
+{
+    "color_alias": "Orange/Yellow",
+    "color_xref": "Yellow Gold"
+},
+{
+    "color_alias": "Purple/Violet",
+    "color_xref": "Dark Lavender"
+},
+{
+    "color_alias": "Purple/Violet",
+    "color_xref": "Lapis"
+},
+{
+    "color_alias": "Purple/Violet",
+    "color_xref": "Lavender Dust"
+},
+{
+    "color_alias": "Purple/Violet",
+    "color_xref": "Lilac"
+},
+{
+    "color_alias": "Purple/Violet",
+    "color_xref": "Orchid"
+},
+{
+    "color_alias": "Purple/Violet",
+    "color_xref": "Oxblood Black"
+},
+{
+    "color_alias": "Purple/Violet",
+    "color_xref": "Plum"
+},
+{
+    "color_alias": "Purple/Violet",
+    "color_xref": "Purple"
+},
+{
+    "color_alias": "Purple/Violet",
+    "color_xref": "Royal Purple"
+},
+{
+    "color_alias": "Purple/Violet",
+    "color_xref": "Storm"
+},
+{
+    "color_alias": "Purple/Violet",
+    "color_xref": "Team Purple"
+},
+{
+    "color_alias": "Red/Pink",
+    "color_xref": "Berry"
+},
+{
+    "color_alias": "Red/Pink",
+    "color_xref": "Brick"
+},
+{
+    "color_alias": "Red/Pink",
+    "color_xref": "Bubble Gum"
+},
+{
+    "color_alias": "Red/Pink",
+    "color_xref": "Canvas Red"
+},
+{
+    "color_alias": "Red/Pink",
+    "color_xref": "Cardinal"
+},
+{
+    "color_alias": "Red/Pink",
+    "color_xref": "Charity Pink"
+},
+{
+    "color_alias": "Red/Pink",
+    "color_xref": "Clay"
+},
+{
+    "color_alias": "Red/Pink",
+    "color_xref": "Fuchsia"
+},
+{
+    "color_alias": "Red/Pink",
+    "color_xref": "Heather Magenta"
+},
+{
+    "color_alias": "Red/Pink",
+    "color_xref": "Maroon"
+},
+{
+    "color_alias": "Red/Pink",
+    "color_xref": "Mauve"
+},
+{
+    "color_alias": "Red/Pink",
+    "color_xref": "Neon Pink"
+},
+{
+    "color_alias": "Red/Pink",
+    "color_xref": "Peach"
+},
+{
+    "color_alias": "Red/Pink",
+    "color_xref": "Pink"
+},
+{
+    "color_alias": "Red/Pink",
+    "color_xref": "Pink Gravel"
+},
+{
+    "color_alias": "Red/Pink",
+    "color_xref": "Poppy"
+},
+{
+    "color_alias": "Red/Pink",
+    "color_xref": "Raspberry"
+},
+{
+    "color_alias": "Red/Pink",
+    "color_xref": "Red"
+},
+{
+    "color_alias": "Red/Pink",
+    "color_xref": "Red Acid Wash"
+},
+{
+    "color_alias": "Red/Pink",
+    "color_xref": "Rust"
+},
+{
+    "color_alias": "Red/Pink",
+    "color_xref": "Soft Pink"
+},
+{
+    "color_alias": "White/Grey/Black",
+    "color_xref": "Ash"
+},
+{
+    "color_alias": "White/Grey/Black",
+    "color_xref": "Asphalt"
+},
+{
+    "color_alias": "White/Grey/Black",
+    "color_xref": "Athletic Grey"
+},
+{
+    "color_alias": "White/Grey/Black",
+    "color_xref": "Athletic Heather"
+},
+{
+    "color_alias": "White/Grey/Black",
+    "color_xref": "Black "
+},
+{
+    "color_alias": "White/Grey/Black",
+    "color_xref": "Black Heather"
+},
+{
+    "color_alias": "White/Grey/Black",
+    "color_xref": "Cement"
+},
+{
+    "color_alias": "White/Grey/Black",
+    "color_xref": "Charcoal Black"
+},
+{
+    "color_alias": "White/Grey/Black",
+    "color_xref": "Cool Grey"
+},
+{
+    "color_alias": "White/Grey/Black",
+    "color_xref": "Dark Grey"
+},
+{
+    "color_alias": "White/Grey/Black",
+    "color_xref": "Deep Heather"
+},
+{
+    "color_alias": "White/Grey/Black",
+    "color_xref": "Granite"
+},
+{
+    "color_alias": "White/Grey/Black",
+    "color_xref": "Grey"
+},
+{
+    "color_alias": "White/Grey/Black",
+    "color_xref": "Grey Acid Fleece"
+},
+{
+    "color_alias": "White/Grey/Black",
+    "color_xref": "Grey Acid Wash"
+},
+{
+    "color_alias": "White/Grey/Black",
+    "color_xref": "Silver"
+},
+{
+    "color_alias": "White/Grey/Black",
+    "color_xref": "Vintage Black"
+},
+{
+    "color_alias": "White/Grey/Black",
+    "color_xref": "Vintage White"
+},
+{
+    "color_alias": "White/Grey/Black",
+    "color_xref": "White"
+},
+{
+    "color_alias": "White/Grey/Black",
+    "color_xref": "White Fleck"
+}
+]
+
+var fetchData = [];
+
+bellaColors.forEach(function (item) {
+    $.ajax({
+        url: `https://www.bellacanvas.com/cgi-bin/live/wam_tmpl/shop_by_color.p?action=getDetails&page=shop_by_color&color_alias=${item.color_alias}&color_xref=${item.color_xref}&site=canvas&layout=Base&nocache=26602&content=json`,
+    }).done(function (response) {
+        var data = response.response.tColor_Item[0]
+        console.log(data);
+        fetchData = [...fetchData, data]
+    });
+});
+
+console.log(JSON.stringify(colors))
+var bellaColors = [];
+[/**/].forEach(function (item) {
+    bellaColors = [...bellaColors, {
+        color_alias: item.color_alias_display,
+        color_xref: item.color_xref_display
+    }]
+})
+console.log(JSON.stringify(bellaColors))
+
+var colors = [];
+
+
+function rgb2cmyk(r, g, b) {
+    var computedC = 0;
+    var computedM = 0;
+    var computedY = 0;
+    var computedK = 0;
+
+    //remove spaces from input RGB values, convert to int
+    var r = parseInt(('' + r).replace(/\s/g, ''), 10);
+    var g = parseInt(('' + g).replace(/\s/g, ''), 10);
+    var b = parseInt(('' + b).replace(/\s/g, ''), 10);
+
+    if (r == null || g == null || b == null ||
+        isNaN(r) || isNaN(g) || isNaN(b)) {
+        console.log('Please enter numeric RGB values!');
+        return;
+    }
+    if (r < 0 || g < 0 || b < 0 || r > 255 || g > 255 || b > 255) {
+        console.log('RGB values must be in the range 0 to 255.');
+        return;
+    }
+
+    // BLACK
+    if (r == 0 && g == 0 && b == 0) {
+        computedK = 1;
+        return [0, 0, 0, 1];
+    }
+
+    computedC = 1 - (r / 255);
+    computedM = 1 - (g / 255);
+    computedY = 1 - (b / 255);
+
+    var minCMY = Math.min(computedC,
+        Math.min(computedM, computedY));
+    computedC = Math.round((computedC - minCMY) / (1 - minCMY) * 100);
+    computedM = Math.round((computedM - minCMY) / (1 - minCMY) * 100);
+    computedY = Math.round((computedY - minCMY) / (1 - minCMY) * 100);
+    computedK = Math.round(minCMY * 100);
+
+    return {
+        c: computedC,
+        m: computedM,
+        y: computedY,
+        k: computedK
+    };
+}
+
+
+function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
+
+const bellaCanvasExtractor = () => {
+    [/* bella-canvas-raw.json here */].forEach(item => {
+
+
+        var color_variation = item.color_variation[0];
+        var hex = null;
+        if (color_variation.multiHex) {
+            // select the second value for accurate color
+            hex = color_variation.hex_value.split(',')[1]
+        } else {
+            hex = color_variation.hex_value;
+        }
+        hex = '#' + hex
+
+        console.log(hex)
+        var rgb = hexToRgb(hex);
+
+        var cmyk = rgb2cmyk(rgb.r, rgb.g, rgb.b)
+        // console.log(hex,rgb,cmyk)
+        defaultStructure({
+            brandCode: 'BEL',
+            brandName: 'Bella Canvas',
+            hex,
+            name: item.color_xref_display,
+            cmyk,
+            rgb,
+            pantone: item.pms,
+            swatchImage1: '/bella/' + item.color_swatch.replace('https://bellacanvas.com//bella/images/color/', ''),
+            colorPatternName: 'Solid'
+        })
+    })
+}
+bellaCanvasExtractor();
+
+function defaultStructure({
+    brandCode,
+    brandName,
+    hex,
+    name,
+    cmyk,
+    rgb,
+    pantone,
+    swatchImage1,
+    colorPatternName
+}) {
+    var color = {
+        brandCode,
+        brandName,
+        hex,
+        name,
+        cmyk: cmyk ? {
+            C: cmyk.c,
+            M: cmyk.m,
+            Y: cmyk.y,
+            K: cmyk.k
+        } : null,
+        rgb: rgb ? {
+            R: rgb.r,
+            G: rgb.g,
+            B: rgb.b
+        } : null,
+        pantone,
+        swatchImage1,
+        colorPatternName
+    }
+    colors = [...colors, color];
+}
+
+console.log(JSON.stringify(colors))
