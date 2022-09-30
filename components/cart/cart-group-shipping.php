487e176007766f44f737675f565976da6d43f335
christianwebdev200 <christian@webdev200.com>
2022-09-30 11:23:51 +0800

    new cart updates

--- components/cart/cart-group-shipping.php
+++ components/cart/cart-group-shipping.php
@@ -6,20 +6,32 @@
     <ul>
         <?php foreach ($shippings as $idx => $shipping) {
 
-            $shippingName = 'shipping-'.$group['id'];
-            $shippingId = $shippingName.'-' . $idx;
+            $shippingName = 'shipping-' . $group['id'];
+            $shippingId = $shippingName . '-' . $idx;
 
         ?>
             <li>
                 <div class="cart-shipping-item">
                     <div class="cart-shipping-item__toggle">
-                        <input type="radio" checked="<?= $shipping['selected'] ?>" name="<?=$shippingName?>" id="<?= $shippingId ?>">
+                        <input type="radio" checked="<?= $shipping['selected'] ?>" name="<?= $shippingName ?>" id="<?= $shippingId ?>">
 
                         <label for="<?= $shippingId ?>"></label>
                     </div>
 
                     <div class="cart-shipping-item__body">
-                        <h4 class="cart-shipping-item__title"><?= $shipping['date'] ?></h4>
+                        <div class="cart-shipping-item__header">
+                            <img src="https://300dev.bulkapparel.com/images/surepost-logo.jpg" alt="" class="cart-shipping-item__image">
+
+                            <h4 class="cart-shipping-item__title">
+                                <?= $shipping['date'] ?>
+                            </h4>
+
+                            <button type="button" class="btn cart-shipping-item__info">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                    <path d="M13 9h-2V7h2m0 10h-2v-6h2m-1-9A10 10 0 002 12a10 10 0 0010 10 10 10 0 0010-10A10 10 0 0012 2z"></path>
+                                </svg>
+                            </button>
+                        </div>
                         <p class="cart-shipping-item__description text-secondary">
                             <?= $shipping['description'] ?>
                         </p>
