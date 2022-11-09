c4ff1ace74acb474f738029a93e97a8729aca9d9
unknown <christian@webdev200.com>
2022-11-09 14:23:49 +0800

    Updates

--- buy-again.php
+++ buy-again.php
@@ -166,6 +166,144 @@
                         ]
                     ]
                 ]
+            ],
+            [
+                "id" => "unique-1111111",
+                "name" => "G500 Gildan T-Shirt 5000 Heavy Cotton 5.3oz",
+                "image" => "16_fm.jpg",
+                "price" => 2.19,
+                "ordered_variants" => [
+                    [
+                        "id" => "gtin-111111",
+                        "color" => "Antique Red",
+                        "hex" => ["#D25168"],
+                        "size" => "L",
+                        "price" => 1.77,
+                    ],
+                    [
+                        "id" => "gtin-2222222",
+                        "color" => "Dark Gray",
+                        "hex" => ["#4D4D52"],
+                        "size" => "L",
+                        "price" => 1.77,
+                    ],
+                    [
+                        "id" => "gtin-3333333",
+                        "color" => "Pink",
+                        "hex" => ["#FFC0CB"],
+                        "size" => "L",
+                        "price" => 1.77,
+                    ],
+                    [
+                        "id" => "gtin-444444444",
+                        "color" => "Violet",
+                        "hex" => ["#EE82EE"],
+                        "size" => "L",
+                        "price" => 1.77,
+                    ],
+                    [
+                        "id" => "gtin-111111",
+                        "color" => "Hello Pink",
+                        "hex" => ["#D25168"],
+                        "size" => "L",
+                        "price" => 1.77,
+                    ],
+                    [
+                        "id" => "gtin-2222222",
+                        "color" => "Dark Gray",
+                        "hex" => ["#4D4D52"],
+                        "size" => "L",
+                        "price" => 1.77,
+                    ],
+                    [
+                        "id" => "gtin-3333333",
+                        "color" => "Pink",
+                        "hex" => ["#FFC0CB"],
+                        "size" => "L",
+                        "price" => 1.77,
+                    ],
+                    [
+                        "id" => "gtin-444444444",
+                        "color" => "Violet",
+                        "hex" => ["#EE82EE"],
+                        "size" => "L",
+                        "price" => 1.77,
+                    ],
+                ],
+                "variants" => [
+                    "colors" => [
+                        [
+                            "id" => 1,
+                            "color" => "Dark Gray",
+                            "hex" => ["#4D4D52"],
+                        ],
+                        [
+                            "id" => 2,
+                            "color" => "Violet",
+                            "hex" => ["#EE82EE"],
+                        ],
+                        [
+                            "id" => 3,
+                            "color" => "Pink",
+                            "hex" => ["#FFC0CB"],
+                        ],
+                        [
+                            "id" => 4,
+                            "color" => "Antique Red",
+                            "hex" => ["#D25168"],
+                        ],
+                        [
+                            "id" => 5,
+                            "color" => "Antique Red",
+                            "hex" => ["#D25168"],
+                        ],
+
+                    ],
+                    "sizes" => [
+                        [
+                            "id" => 1,
+                            "size" => "S",
+                            "price" => 2.19,
+                            "stocks" => 216533,
+                        ],
+                        [
+                            "id" => 2,
+                            "size" => "M",
+                            "price" => 2.19,
+                            "stocks" => 216533
+                        ],
+                        [
+                            "id" => 3,
+                            "size" => "L",
+                            "price" => 2.19,
+                            "stocks" => 216533
+                        ],
+                        [
+                            "id" => 4,
+                            "size" => "XL",
+                            "price" => 2.19,
+                            "stocks" => 216533
+                        ],
+                        [
+                            "id" => 5,
+                            "size" => "2XL",
+                            "price" => 2.19,
+                            "stocks" => 216533
+                        ],
+                        [
+                            "id" => 5,
+                            "size" => "2XL",
+                            "price" => 2.19,
+                            "stocks" => 216533
+                        ],
+                        [
+                            "id" => 5,
+                            "size" => "2XL",
+                            "price" => 2.19,
+                            "stocks" => 216533
+                        ]
+                    ]
+                ]
             ]
         ];
 
@@ -198,6 +336,30 @@
 
                 <div class="buy-again__list">
                     <?php template('./components/buy-again/buy-again-list.php', ['items' => $data]); ?>
+                </div>
+            </div>
+        </div>
+
+        <div class="modal-dialog modal-dialog-centered" role="document" style="width: 350px; display:none;">
+            <div class="modal-content">
+                <div class="modal-header pb-0">
+                    <h3 class="modal-title">
+                        Please enter quantity
+                    </h3>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>
+                        Enter the quantity you would like to order in at least one of the boxes that looks this.
+                    </p>
+
+                    <img src="<?= base_url_site ?>images/indication-box-new2.jpg" loading="lazy" alt="enter quantity on text field">
+                </div>
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary">Okay</button>
                 </div>
             </div>
         </div>
